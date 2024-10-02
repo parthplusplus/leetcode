2a7c730a404d947230a8985dd89eca75bc0fca6b
ShahManali99 <52094953+ShahManali99@users.noreply.github.com>
2024-10-02 14:45:13 -0700

    Delete Manali/extra directory

--- Manali/extra/RemoveDuplicatesFromSortedArray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        ct = 1
-        for i in range(1,len(nums)):
-            if nums[i]!=nums[i-1]:
-                nums[ct]= nums[i]
-                ct+=1
-        return ct
--- Manali/extra/RemoveElement.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        ct = 0
-        for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[ct] = nums[i]
-                ct+=1
-        return ct
