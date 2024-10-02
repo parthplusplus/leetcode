4ee972b61892496a45e2addf28ed22d42cf7680c
ShahManali99 <52094953+ShahManali99@users.noreply.github.com>
2024-10-02 14:44:44 -0700

    Delete Manali/Week2 directory

--- Manali/Week2/GroupAnagrams.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for str in strs:
-            sorted_word = ''.join(sorted(str))
-            res[sorted_word].append(str)
-        return list(res.values())
-    
