270c4679f63a93cd19d8cc6dd68c8ab284b05908
ShahManali99 <52094953+ShahManali99@users.noreply.github.com>
2024-10-09 15:57:30 -0700

    Update ProductOfArrayExceptSelf.py

--- Manali/ArraysAndHashing/ProductOfArrayExceptSelf.py
+++ Manali/ArraysAndHashing/ProductOfArrayExceptSelf.py
@@ -13,3 +13,23 @@ class Solution:
     
 TC: O(n)
 SC: O(1)
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1]
+        for i in range(len(nums)):
+            ans.append(ans[i]*nums[i])
+
+        res = [1] * (len(nums)+1)
+        for i in range(len(nums)-1,-1,-1):
+            res[i]=res[i+1]*nums[i]   
+
+        out = [0]*len(nums)
+        for i in range(0,len(out)):
+            out[i] = ans[i]*res[i+1]
+        return out
+
+
+TC: O(n)
+SC: O(n)
+        
