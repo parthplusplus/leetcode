44f29fa71f94449b85ff9a953d06ff8829385191
ShahManali99 <52094953+ShahManali99@users.noreply.github.com>
2024-10-09 11:06:52 -0700

    Update TopKFrequentElements.py

--- Manali/ArraysAndHashing/TopKFrequentElements.py
+++ Manali/ArraysAndHashing/TopKFrequentElements.py
@@ -14,3 +14,12 @@ class Solution:
 
 TC: O(n)
 SC: O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k==len(nums): return nums
+        freq = Counter(nums)
+        return heapq.nlargest(k, freq.keys(), key = freq.get)
+
+TC: O(n log k)
+SC: O(n + k)
